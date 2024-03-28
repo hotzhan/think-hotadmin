2948c603b789c2248cf20fd88a9ba4d3978e3af4
hotzhan <770524521@qq.com>
2024-03-28 11:45:09 +0800

    去除无效文件

--- app/index/config/hot_index.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-	//网站设置
-	'base' => [
-		//网站名称
-		'sitename' => 'HotAdmin后台管理系统',
-	],
-
-];
--- app/index/route/route.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use think\facade\Route;
-
-// route/route.php
-//Route::get('api/token', 'Index/token');
